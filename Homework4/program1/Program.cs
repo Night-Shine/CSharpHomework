53a34cbf6c803abf01be7bf9e69e4781fab80267
Night-Shine <43108892+Night-Shine@users.noreply.github.com>
2018-10-08 20:13:50 +0800

    Add files via upload

---
+++ Homework4/program1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+
+        public class AlarmEventArgs : EventArgs {
+            public int a;
+            public int b;
+            public int c;
+        }
+        public class AlarmClock {
+            public event AlarmEventHandler AlarmEvent;
+            private AlarmEventArgs alarm;
+
+            private int Hour;
+            private int Minute;
+            private int Second;
+
+            public AlarmClock(){
+                this.Hour = DateTime.Now.Hour;
+                this.Minute = DateTime.Now.Minute;
+                this.Second = DateTime.Now.Second;
+
+                alarm = new AlarmEventArgs();
+                AlarmEvent += Alarm;
+            }
+
+            public void SetAlarmClock() {
+                try {
+                    Console.WriteLine("请输入闹钟时间（时、分、秒）：");
+                    int a = Int32.Parse(Console.ReadLine());
+                    int b = Int32.Parse(Console.ReadLine());
+                    int c = Int32.Parse(Console.ReadLine());
+                    alarm.a = a;
+                    alarm.b = b;
+                    alarm.c = c;
+                }
+                catch {
+                    Console.WriteLine("输入错误请重新输入：");
+                    SetAlarmClock();
+                }
+            }
+
+            public void Update(){
+                do
+                {
+                    this.Hour = DateTime.Now.Hour;
+                    this.Minute = DateTime.Now.Minute;
+                    this.Second = DateTime.Now.Second;
+                } while (!(this.Hour == alarm.a && this.Minute == alarm.b && this.Second == alarm.c));
+                {
+                    AlarmEvent(this, alarm);
+                }
+            }
+
+            private void Alarm(object sender, AlarmEventArgs e)
+            {
+                Console.WriteLine("闹钟时间到了。。。。。。");
+                Console.WriteLine("当前时间：" + this.Hour + ":" + this.Minute + ":" + this.Second);
+            }
+        }
+        static void Main(string[] args)
+        {
+            AlarmClock AlarmClock = new AlarmClock();
+            AlarmClock.SetAlarmClock();
+            AlarmClock.Update();
+            Console.WriteLine("按任意键退出...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework4/program2/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Program
+    {
+        class Order
+        {
+            public OrderDetails Detail = new OrderDetails();
+
+            public void SetDate()
+            {
+                Console.WriteLine("请输入订单信息：");
+                Console.Write("订单号：");
+                try {
+                    int Num = Int32.Parse(Console.ReadLine());
+                    Detail.Num = Num;
+                } catch {
+                    Console.WriteLine("输入订单号错误。");
+                }
+                Console.Write("客户名：");
+                string CustomerName = Console.ReadLine();
+                Detail.CutomerName = CustomerName;
+                Console.Write("商品名：");
+                string ProductName = Console.ReadLine();
+                Detail.ProductName = ProductName;
+            }
+        }
+
+        class OrderDetails
+        {
+            public int Num;
+            public string CutomerName = "";
+            public string ProductName = "";
+        }
+
+        class OrderService {
+            static public List<Order> orderList = new List<Order>();
+
+            public int findOrder(string num,int b=0) {
+                int a = 0;
+                foreach (Order Order in orderList)
+                {
+                    a++;
+                }
+                if (num == "1")
+                {
+                    Console.Write("请输入要查询的订单号：");
+                    int s = Int32.Parse(Console.ReadLine());
+                    for (int i = 0; i < a; i++) {
+                        if (orderList[i].Detail.Num == s) {
+                            b = i;
+                        }
+                    }
+                    if (b == 0) {
+                        Console.Write("查无可证。");
+                    }
+                }
+                else if (num == "2")
+                {
+                    Console.Write("请输入要查询的商品名称：");
+                    string s = Console.ReadLine();
+                    for (int i = 0; i < a; i++)
+                    {
+                        if (orderList[i].Detail.ProductName == s)
+                        {
+                            b = i;
+                        }
+                    }
+                    if (b == 0)
+                    {
+                        Console.Write("查无可证。");
+                    }
+                }
+                else if (num == "3")
+                {
+                    Console.Write("请输入要查询的客户名称：");
+                    string s = Console.ReadLine();
+                    for (int i = 0; i < a; i++)
+                    {
+                        if (orderList[i].Detail.CutomerName == s)
+                        {
+                            b = i;
+                        }
+                    }
+                    if (b == 0)
+                    {
+                        Console.Write("查无可证。");
+                    }
+                }
+                else
+                {
+                    Console.Write("查无可证。");
+                }
+                return b;
+            }
+
+            public void getOrder(string num) {
+                if (num == "1")
+                {
+                    Order a = new Order();
+                    a.SetDate();
+                    orderList.Add(a);
+                    Console.WriteLine("请继续指令操作或者按任意指令外的键退出：");
+                    this.getOrder(Console.ReadLine());
+                }
+                else if (num == "2")
+                {
+                    Console.WriteLine("请输入查询订单的条件数字（订单号、商品名称、客户名称以此为1、2、3）：");
+                    int b = findOrder(Console.ReadLine(), 0);
+                    orderList.Remove(orderList[b]);
+                    Console.WriteLine("请继续指令操作或者按任意指令外的键退出：");
+                    this.getOrder(Console.ReadLine());
+                }
+                else if (num == "3")
+                {
+                    Console.WriteLine("请输入查询订单的条件数字（订单号、商品名称、客户名称以此为1、2、3）：");
+                    int b = findOrder(Console.ReadLine(), 0);
+                    Console.WriteLine("请继续指令操作或者按任意指令外的键退出：");
+                    this.getOrder(Console.ReadLine());
+                }
+                else if (num == "4")
+                {
+                    Console.WriteLine("请输入查询订单的条件数字（订单号、商品名称、客户名称以此为1、2、3）：");
+                    int b = findOrder(Console.ReadLine(), 0);
+                    Console.WriteLine(orderList[b]);
+                    Console.WriteLine("请继续指令操作或者按任意指令外的键退出：");
+                    this.getOrder(Console.ReadLine());
+                }
+                else
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            OrderService order = new OrderService();
+            Console.WriteLine("请输入相应的指令数字（添加订单、删除订单、修改订单、查询订单分别为1、2、3、4）或者按任意指令外的键退出：");
+            order.getOrder(Console.ReadLine());
+        }
+    }
+}
