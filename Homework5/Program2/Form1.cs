2943dabfa2f5ffd4a09b0f458b68a6a34afceb75
Night-Shine <43108892+Night-Shine@users.noreply.github.com>
2018-10-17 20:33:51 +0800

    Add files via upload

---
+++ Homework5/Program2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+        }
+        private Graphics graphics;
+        double per1 = 0.6;
+        double per2 = 0.7;
+
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th) {
+
+            if (n == 0) return;
+
+            double th1 = double.Parse(textBox2.Text) * Math.PI / 180;
+            double th2 = double.Parse(textBox3.Text) * Math.PI / 180;
+            double k = double.Parse(textBox1.Text);
+            double x1 = x0 + leng * 1.3 * k * Math.Cos(th);
+            double y1 = y0 + leng * 1.3 * k * Math.Sin(th);
+            double x2 = x0 + leng * k * Math.Cos(th);
+            double y2 = y0 + leng * k * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1);
+            drawLine(x0, y0, x2, y2);
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x2, y2, per2 * leng, th - th2);
+        }
+
+        void drawLine(double x0, double y0, double x1, double y1) {
+            float wid = float.Parse(textBox4.Text);
+            Pen p2 = new Pen(Color.SkyBlue, wid);
+
+            graphics.DrawLine(p2,(int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
