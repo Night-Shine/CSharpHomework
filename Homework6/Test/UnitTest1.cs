1aa89c410a536bef35da2a5e79ffa56a6d3eb40b
Night-Shine <43108892+Night-Shine@users.noreply.github.com>
2018-10-24 19:27:13 +0800

    Add files via upload

---
+++ Homework6/Test/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+using program1;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShowListtest()
+        {
+            List<Program.Order> testList = new List<Program.Order>();
+            foreach (Program.Order order in testList)
+            {
+                Console.WriteLine(order.ToString());
+            }
+            Assert.IsNotNull(testList);
+            Assert.IsNull(testList);
+        }
+        [TestMethod()]
+        public void addOrdertest()
+        {
+            List<Program.Order> testList = new List<Program.Order>();
+            Program.Order order = new Program.Order();
+            order.Detail.Num = 0;
+            testList.Add(order);
+            Assert.IsNotNull(testList);
+        }
+        [TestMethod()]
+        public void deletOrderTest()
+        {
+            List<Program.Order> testList = new List<Program.Order>();
+            testList.RemoveAt(1);
+            Assert.IsNull(testList);
+            Assert.IsNotNull(testList);
+        }
+        [TestMethod()]
+        public void ChangeListTest()
+        {
+            List<Program.Order> testList = new List<Program.Order>();
+            Program.Order order = new Program.Order();
+            testList.RemoveAt(1);
+            order.Detail.Num = 1;
+            Assert.IsNull(testList);
+            Assert.IsNotNull(testList);
+        }
+        [TestMethod()]
+        public void SearchListTest()
+        {
+            List<Program.Order> testList = new List<Program.Order>();
+            Program.Order order = new Program.Order();
+            order.Detail.CutomerName = "122";
+            order.Detail.ProductName = "111";
+            order.Detail.Num = 1;
+            Assert.IsNull(testList);
+            Assert.IsNotNull(testList);
+        }
+    }
+}
